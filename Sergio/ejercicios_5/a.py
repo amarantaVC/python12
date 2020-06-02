cb2b87ea22943a9058c7587a16af3424e349f9e8
sergio <sergioamaro18@gmail.com>
2020-06-02 15:42:14 -0400

    5a listo

---
+++ Sergio/ejercicios_5/a.py
@@ -0,0 +1,30 @@
+#Dado un entero N, calcular la suma de los numeros primos menores que N. 
+
+#Funcion es primo
+def esPrimo(num):
+
+    esPrimo = True
+    i = 2
+
+    while (esPrimo and i < num):
+        if (num % i == 0):
+            esPrimo = False
+        i += 1
+
+    return esPrimo
+
+# N > -1
+N = int(input("Ingrese el valor de N = "))
+while (N < 0):
+    N = int(input("Ingrese el valor de N = "))
+
+lista, primos, suma = [0, 1], [], 0
+
+for i in range(2, N):
+    lista.append(i)
+    if(esPrimo(lista[i])):
+        suma += lista[i]
+        primos.append(lista[i])
+    
+print(primos)
+print(suma)
