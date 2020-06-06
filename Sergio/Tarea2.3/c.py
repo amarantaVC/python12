7435163dd859dd391473dc89f8123dcaf171feac
sergio <sergioamaro18@gmail.com>
2020-06-06 09:49:29 -0400

    Suma de vectores

--- Sergio/Tarea2.3/c.py
+++ Sergio/Tarea2.3/c.py
@@ -1,1 +1,16 @@
 #Dados dos vectores A y B, calcular un tercer vector C que almacena la suma de ellos. Suponga que los vectores se almacenan en dos secuencias de valores reales
+
+def sumaVectores(a, b):
+    i = 0
+    suma = []
+    while (i < len(a)):
+        suma.append(a[i]+b[i])
+        i += 1
+    
+    return suma
+
+N = int(input('Ingrese el valor de N = '))
+A = [int(input('A[' + str(i) + '] = ')) for i in range(0, N)]
+B = [int(input('A[' + str(i) + '] = ')) for i in range(0, N)]
+
+print('Vector C = '+ str(sumaVectores(A, B)))
